ba5c978db1e9f98b4f37d32bf23f6d781e812ab3
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-23 16:15:23 +0800

    Добавили второе решение Task_3

--- Семинары/Онлайн/23.09.22/Task_3.py
+++ Семинары/Онлайн/23.09.22/Task_3.py
@@ -2,10 +2,14 @@
 # Примеры:
 #         ◦ 5 -> -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
 
-N = int(input('Введите число - '))
-out = ''
-for i in range(-N, N):
-    out = out + str(i)
-    out = out + ', '
-out = out + str(N)
-print(out)
+# N = int(input('Введите число - '))
+# out = ''
+# for i in range(-N, N):
+#     out = out + str(i)
+#     out = out + ', '
+# out = out + str(N)
+# print(out)
+
+N = int(input())
+for i in range(-N, N + 1):
+    print(i, end=' ')
