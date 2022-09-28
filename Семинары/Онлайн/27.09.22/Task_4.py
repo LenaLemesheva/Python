463d1f4530054b97bd50de213db87705ee255201
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-28 13:18:24 +0800

    Добавили второе решение Task_4

--- Семинары/Онлайн/27.09.22/Task_4.py
+++ Семинары/Онлайн/27.09.22/Task_4.py
@@ -1,7 +1,18 @@
 # Напишите программу, которая проверяет пятизначное число на палиндром.
 
-paltus = input('Введите число - ')
-if paltus[:2] == paltus[:2:-1]:
-    print('палиндром')
+# paltus = input('Введите число - ')
+# if paltus[:2] == paltus[:2:-1]:
+#     print('палиндром')
+# else:
+#     print('не палиндром')
+
+number = '123215'
+palindrome = True
+for i in range(len(number) // 2):
+    if number[i] != number[-i-1]:
+        palindrome = False
+        break
+if palindrome:
+    print('Палиндром')
 else:
-    print('не палиндром')
+    print('Не палиндром')
