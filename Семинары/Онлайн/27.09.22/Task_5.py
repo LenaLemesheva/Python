35ed3300a0db23d90e6d8b10daf4527d12c260d5
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-28 13:42:56 +0800

    Добавили третье решение Task_5

--- Семинары/Онлайн/27.09.22/Task_5.py
+++ Семинары/Онлайн/27.09.22/Task_5.py
@@ -3,9 +3,11 @@
 # a = 123
 # print(a // 100 * 10 + a %10)
 
-number = 123
-n1 = number // 100
-n2 = number % 10
-res = n1 * 10 + n2
-print(res)
+# number = 123
+# n1 = number // 100
+# n2 = number % 10
+# res = n1 * 10 + n2
+# print(res)
 
+number = str(123)
+print(number[0] + number[2])
