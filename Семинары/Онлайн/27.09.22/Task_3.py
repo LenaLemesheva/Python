88e0c3dffee18b0cbe9b4b32073791a02a675534
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-28 13:09:28 +0800

    Добавили второе решение Task_3

--- Семинары/Онлайн/27.09.22/Task_3.py
+++ Семинары/Онлайн/27.09.22/Task_3.py
@@ -1,7 +1,11 @@
 # Напишите программу, которая принимает на вход число N и выдаёт 
 # последовательность из N членов.
 
-n = int(input('Введите число - '))
-for i in range(n):
-    result = (-3) ** i
-    print(result, end = " ")
+# n = int(input('Введите число - '))
+# for i in range(n):
+#     result = (-3) ** i
+#     print(result, end = " ")
+
+n = 5
+for i in range(0, n):
+    print((-3) ** i)
