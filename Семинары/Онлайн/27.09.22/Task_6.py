4af82b1d319e6c2e549f5aed5fccf8b0cb0696aa
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-28 14:04:04 +0800

    Добавили второе решение Task_6

--- Семинары/Онлайн/27.09.22/Task_6.py
+++ Семинары/Онлайн/27.09.22/Task_6.py
@@ -1,6 +1,21 @@
 # Напишите программу, в которой пользователь будет задавать две строки, 
 # а программа - определять количество вхождений одной строки в другой.
 
+# s = input('введите буквы: ')
+# s1 = input('введите буквы: ')
+# print(s.count(s1))
+
+from itertools import count
+
+
 s = input('введите буквы: ')
 s1 = input('введите буквы: ')
-print(s.count(s1))
+count = 0
+i = 0
+while i < len(s):
+    if s[i:i + len(s1)] == s1:
+        count += 1
+        i += len(s1)
+    else:
+        i += 1
+print(count)
