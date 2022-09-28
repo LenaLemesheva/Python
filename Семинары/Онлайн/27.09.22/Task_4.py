c0c27dbc0deccd257f9b0cdd15213d17b834c539
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-28 13:22:32 +0800

    Добавили третье решение Task_4

--- Семинары/Онлайн/27.09.22/Task_4.py
+++ Семинары/Онлайн/27.09.22/Task_4.py
@@ -6,13 +6,16 @@
 # else:
 #     print('не палиндром')
 
-number = '123215'
-palindrome = True
-for i in range(len(number) // 2):
-    if number[i] != number[-i-1]:
-        palindrome = False
-        break
-if palindrome:
-    print('Палиндром')
-else:
-    print('Не палиндром')
+# number = '123215'
+# palindrome = True
+# for i in range(len(number) // 2):
+#     if number[i] != number[-i-1]:
+#         palindrome = False
+#         break
+# if palindrome:
+#     print('Палиндром')
+# else:
+#     print('Не палиндром')
+
+number = '12321'
+print(number == number[::-1])
