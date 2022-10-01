3b2221b7809516699e978f75bdb3a969bcc1b147
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-01 17:22:58 +0800

    Добавили второе решение Task_3

--- Семинары/Онлайн/30.09.22/Task_3.py
+++ Семинары/Онлайн/30.09.22/Task_3.py
@@ -1,11 +1,21 @@
 # Реализуйте алгоритм задания случайных чисел без использования встроенного
 # генератора псевдослучайных чисел.
 
-from datetime import *
+# from datetime import *
 
 
-def k():
-    return(datetime.now().microsecond)
-a = int(input("Введите размерность: "))
-res = int(k()/10 ** (6 - a))
-print(res)
+# def k():
+#     return(datetime.now().microsecond)
+# a = int(input("Введите размерность: "))
+# res = int(k()/10 ** (6 - a))
+# print(res)
+
+from datetime import datetime
+
+
+def num_random(n):
+    number = datetime.now().microsecond
+    return number % n
+
+
+print(num_random(100))
