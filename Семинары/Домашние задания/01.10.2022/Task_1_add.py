3ec649a2a3c95bbc0ece79bd62b6d4505176bf2c
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-03 07:50:08 +0800

    Исправили Task_1_add

--- Семинары/Домашние задания/01.10.2022/Task_1_add.py
+++ Семинары/Домашние задания/01.10.2022/Task_1_add.py
@@ -2,18 +2,7 @@
 # Пример:
 # 1 5 2 2 2 4 - 4.
 
-# Вариант 1
-
-plenty = set(input('Введите числа через пробел - '))
-print(plenty)
-N = 0
-for i in plenty:
-    N += 1
-print(N - 1) # чтобы убрать пробел
-
-# Вариант 2
-
-plenty = set(input('Введите числа без пробела - '))
+plenty = set(input('Введите числа через пробел - ').split())
 print(plenty)
 N = 0
 for i in plenty:
