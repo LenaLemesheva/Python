d2c62e11ddea89294f58f4cd4fa31e6491c0e314
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-02 17:17:19 +0800

    Task_5

---
+++ Семинары/Домашние задания/01.10.2022/Task_5.py
@@ -0,0 +1,16 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример:
+# - для k = 8 список будет выглядеть так: 
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
+
+k = int(input('Введите число k - '))
+fib1 = fib2 = 1
+list = [fib1, fib2]
+for i in range(2, k):
+    fib1, fib2 = fib2, fib1 + fib2
+    list.append(fib2)
+fib1 = fib2 = 1
+for i in range(-k, 1):
+    fib1, fib2 = fib2, fib1 - fib2
+    list.insert(0, fib2)  # добавили 0
+print(list)
