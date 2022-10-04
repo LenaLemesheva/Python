fcf15ad1d2944aa226a4a983f4f93e9250e0faa4
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-04 17:03:49 +0800

    Добавили второе решение Task_6

--- Семинары/Онлайн/04.10.22/Task_6.py
+++ Семинары/Онлайн/04.10.22/Task_6.py
@@ -5,20 +5,30 @@
 # 1                                     -       banana
 # apple orange banana banana orange
 
-a = 'apple orange banana banana orange'
-a = a.split()
-b = {}
-max = 0
-word = "ZZZZZZ"
-for i in range(len(a)):
-    if a[i] in b.keys():
-        b[a[i]] += 1
-        if (b[a[i]]) > max:
-            max = b[a[i]]
-    else:
-        b[a[i]] = 1
+# a = 'apple orange banana banana orange'
+# a = a.split()
+# b = {}
+# max = 0
+# word = "ZZZZZZ"
+# for i in range(len(a)):
+#     if a[i] in b.keys():
+#         b[a[i]] += 1
+#         if (b[a[i]]) > max:
+#             max = b[a[i]]
+#     else:
+#         b[a[i]] = 1
 
-for elem in b:
-    if (b[elem] == max) and (elem < word):
-        word = elem
-print(word)
+# for elem in b:
+#     if (b[elem] == max) and (elem < word):
+#         word = elem
+# print(word)
+
+counter = {}
+for i in range(int(input())):
+    line = input().split()
+    for word in line:
+        counter[word] = counter.get(word, 0) + 1
+         
+max_count = max(counter.values())
+most_frequent = [k for k, v in counter.items() if v == max_count]
+print(min(most_frequent))
