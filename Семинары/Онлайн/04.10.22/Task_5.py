f2a0b4c8fd252feac14e88be19f6f65d1f7ddf97
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-04 16:29:48 +0800

    Добавили второе решение Task_5

--- Семинары/Онлайн/04.10.22/Task_5.py
+++ Семинары/Онлайн/04.10.22/Task_5.py
@@ -3,23 +3,31 @@
 # Для слова из словаря, записанного в последней строке, определите его синоним.
 # Пример:
 # 3                 -         Bye
-# Hello Hi
+# Hello Hi Bye Goodbye List Array Goodbye
 # Bye Goodbye
 # List Array
 # Goodbye
 
-vocab = {}
-text = open('text.txt', 'r')
-for line in text:
-    a = line.split()
-    if len(a) == 2:
-        vocab[a[0]] = a[1]
-    if len(a) == 1:
-        word = a[0]
-text.close()
-print (f"Ищем синоним слова: {word}")
-for k, v in vocab.items():
-    if v == word:
-        print(k)
-    if k == word:
-        print(v)
+# vocab = {}
+# text = open('text.txt', 'r')
+# for line in text:
+#     a = line.split()
+#     if len(a) == 2:
+#         vocab[a[0]] = a[1]
+#     if len(a) == 1:
+#         word = a[0]
+# text.close()
+# print (f"Ищем синоним слова: {word}")
+# for k, v in vocab.items():
+#     if v == word:
+#         print(k)
+#     if k == word:
+#         print(v)
+
+n = int(input())
+d = {}
+for i in range(n):
+    first, second = input().split()
+    d[first] = second
+    d[second] = first
+print(d[input()])
