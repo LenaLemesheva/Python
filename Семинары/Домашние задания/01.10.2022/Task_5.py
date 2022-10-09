fa7b091de2be953bb8ecf8d503599a809ecbbe43
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-09 13:27:14 +0800

    Добавили второе решение Task_5

--- Семинары/Домашние задания/01.10.2022/Task_5.py
+++ Семинары/Домашние задания/01.10.2022/Task_5.py
@@ -14,3 +14,13 @@ for i in range(-k, 1):
     fib1, fib2 = fib2, fib1 - fib2
     list.insert(0, fib2)  # добавили 0
 print(list)
+
+
+n = 8
+fib = [0, 1]
+for i in range(2, n + 1):
+    fib.append(fib[-1] + fib[-2])
+for i in range(n):
+    # fib = [fib[1] - fib[0] + fib]
+    fib.insert(0, fib[1] - fib[0])
+print(fib)
