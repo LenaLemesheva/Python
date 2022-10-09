b35a2c038ed534990733bf1869540e8c2e3457c4
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-09 15:21:57 +0800

    Добавили второе решение Task_3

--- Семинары/Онлайн/08.10.22/Task_3.py
+++ Семинары/Онлайн/08.10.22/Task_3.py
@@ -4,16 +4,22 @@
 # triangle(1, 1, 2)   -   Это не треугольник
 # triangle(7, 6, 10)  -   Это треугольник
 
-a = int(input())
-b = int(input())
-c = int(input())
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+
+# def triangle(a, b, c):
+#     flag = False
+#     if (a+b) > c and (a+c) > b and (c+b) > a:
+#         flag = True
+#     return 'yes' if flag else 'no'
+
+
+# print(triangle(a, b, c))
+
 
 
 def triangle(a, b, c):
-    flag = False
-    if (a+b) > c and (a+c) > b and (c+b) > a:
-        flag = True
-    return 'yes' if flag else 'no'
-
-
-print(triangle(a, b, c))
+    return('Это - не треугольник', 'Это - треугольник')[(a + b > c) and (a + c > b) and (b + c > a)]
+print(triangle(int(input()), int(input()), int(input())))
