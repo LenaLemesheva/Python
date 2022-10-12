60c9945bbce4bdd2f9fc1dd06faad3e0386dd65c
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-12 13:13:39 +0800

    Добавили решение Task_3

--- Семинары/Онлайн/12.10.22/Task_3.py
+++ Семинары/Онлайн/12.10.22/Task_3.py
@@ -14,9 +14,13 @@
 # from itertools import zip_longest  # Функция zip_longest работает аналогично встроенной функции zip
                                    # но не останавливается на самом коротком итерируемом объекте
 
+from itertools import zip_longest
+
+
 users = ['user1','user2','user3','user4','user5']
 ids = [4, 5, 9, 14, 7]
 salary = [111,222,333]
 temp = [list(i) for i in zip(users, ids, salary)]
+# temp = [list(i) for i in zip_longest(users, ids, salary, fillvalue='')]  # если список разной длины
 print(temp)
 print(list(zip(*temp))) # * для распаковки итерируемых объектов в список/кортеж
