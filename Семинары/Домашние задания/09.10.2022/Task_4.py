2cc60e92398c0c5c5be324d311c0ba020e79abc7
ElenaLemesheva <lenleolem@yandex.ru>
2022-10-12 19:31:48 +0800

    Task_4

--- Семинары/Домашние задания/09.10.2022/Task_4.py
+++ Семинары/Домашние задания/09.10.2022/Task_4.py
@@ -5,5 +5,36 @@
 # Текст после кодировки: 12W1B12W3B24W1B14W
 # Текст после дешифровки: 
 # WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
-# Входные и выходные данные хранятся в отдельных текстовых файлах.
 
+
+txt = input('Введите текст для кодировки - ')
+
+
+def compression(text):
+    count = 1
+    res = ''
+    for i in range(len(text)-1):
+        if text[i] == text[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + text[i]
+            count = 1
+    if count > 1 or (text[len(text)-2] != text[-1]):
+        res = res + str(count) + text[-1]
+    return res
+
+
+def recovery(text):
+    number = ''
+    res = ''
+    for i in range(len(text)):
+        if not text[i].isalpha():
+            number += text[i]
+        else:
+            res = res + text[i] * int(number)
+            number = ''
+    return res
+
+
+print(f'Текст после кодировки - {compression(txt)}')
+print(f'Текст после дешифровки - {recovery(compression(txt))}')
